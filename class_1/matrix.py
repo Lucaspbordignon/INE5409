67433b37cacf9e043678af381f0080a2d5a1f1aa
Lucas Bordignon <lucaspbordignon99@gmail.com>
2016-08-18 23:27:26 -0300

    Gauss function added.

---
+++ class_1/matrix.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+matrix = [[np.pi,       np.e,           np.sqrt(2),         2*np.pi, 1],
+          [np.sqrt(3),  np.sqrt(10),    -1/np.sqrt(3),      3,      2],
+          [np.e,        -(np.sqrt(2)),  1/(np.sqrt(10)),    -1/2,   3],
+          [43,          1/2,            np.sqrt(9),         19,     -1]]
+
+
+def fgauss(matrix):
+    size = len(matrix)
+    for k in range(size):
+        for i in range(k+1, size):
+            for j in range(k+1, size+1):
+                aux = matrix[i][k]/matrix[k][k]
+                matrix[i][j] = round(matrix[i][j] - aux * matrix[k][j], 4)
+            matrix[i][k] = 0.00000
+    matrix_show(matrix)
+
+
+def matrix_show(matrix):
+    for i in matrix:
+        print('[ ', end='')
+        for j in i:
+            print("%.5f " % j, end='')
+        print(" ]\n")
+
+if __name__ == '__main__':
+    fgauss(matrix)
