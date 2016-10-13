2ea242ca7326eec99cf9b3486b6e700f779a90ae
Lucas Bordignon <lucaspbordignon99@gmail.com>
2016-10-12 22:45:24 -0300

    Second exercise added

--- Task_2/Polynom.m
+++ Task_2/Polynom.m
@@ -9,3 +9,14 @@ P = [1, (-3.9 -1i), (5.7 + 3.9i), (-3.7 - 5.7i), (0.9 + 3.7i), (0 - 0.9i)];
 disp("Questao 1:")
 Rlim = 1e-3
 Roots = fRoots(P)
+
+% Questao 2
+disp("Questao 2:")
+Roots_octave = roots(P)
+Roots_wolfram = [0.9, 0.99999, 1i, 1.00001 + 8.96723e-6i, 1.00001 - 8.96723e-6i]
+
+% Questao 3
+disp("Questao 3: Exercicio 3.9")
+
+% Questao 4
+disp("Questao 4: Exercicio 4.3")
